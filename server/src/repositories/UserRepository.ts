aca5c1605e902b121b7707b7e903b460bf197151
Felix <felixlidaili98@gmail.com>
2024-01-16 02:20:56 +0800

    feat: create user seed data

---
+++ server/src/repositories/UserRepository.ts
@@ -0,0 +1,13 @@
+import { User } from "../entities/User";
+import { AppDataSource } from "../config/data-source";
+
+// Interacts database open close
+export class UserRepository {
+    async getAllUsers() {
+        return await AppDataSource.getRepository(User).find()
+    }
+
+    async createUser(user: User) {
+        return await AppDataSource.getRepository(User).save(user)
+    }
+}
