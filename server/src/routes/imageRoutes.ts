da4704b66adef1559486368be37798ffe19d9952
Xun Yi <xunyi.work@gmail.com>
2024-01-24 23:08:09 +0800

    add update image on backend

--- server/src/routes/imageRoutes.ts
+++ server/src/routes/imageRoutes.ts
@@ -61,5 +61,45 @@ router.post('/', function (req: Request, res: Response, next: NextFunction) {
         });
     });
 });
+router.put('/:imageId', function (req: Request, res: Response, next: NextFunction) {
+  upload(req, res, async function (err) {
+        if (err instanceof MulterError) {
+            return res.status(500).json({ error: 'MulterError', message: err.message });
+        } else if (err) {
+            return res.status(500).json({ error: 'UnknownError', message: err.message });
+        }
+
+        const uploadedFiles: Express.Multer.File[] = req.files as Express.Multer.File[];
+
+        if (!uploadedFiles || uploadedFiles.length === 0) {
+            return res.status(400).json({ success: false, message: 'No file uploaded' });
+        }
+
+        const imageId = req.params.imageId;
+        const uploadedImagesPath = './uploaded_images';
+        const imagePath = `${uploadedImagesPath}/${imageId}`;
+
+        try {
+          // Check if the file exists
+          if (fs.existsSync(imagePath)) {
+            // Remove the existing image file
+            fs.unlinkSync(imagePath);
+          }
+
+          // Move the new file to the destination folder with the existing imageId
+          fs.renameSync(uploadedFiles[0].path, imagePath);
+
+          return res.status(200).json({
+            success: true,
+            message: 'File updated successfully',
+            filename: uploadedFiles[0].filename,
+          });
+        } catch (error) {
+          console.error('Error:', error);
+          return res.status(500).json({ success: false, message: 'Internal server error' });
+        }
+    });
+});
+
 
 export default router;
