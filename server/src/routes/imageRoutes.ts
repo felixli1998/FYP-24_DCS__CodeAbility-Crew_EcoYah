ad0aeb6998a3e60c0b69ad4ba398de507218b4f3
Xun Yi <xunyi.work@gmail.com>
2024-01-25 10:44:41 +0800

    change uploaded image to be saved as uuid

--- server/src/routes/imageRoutes.ts
+++ server/src/routes/imageRoutes.ts
@@ -46,14 +46,11 @@ router.post('/', function (req: Request, res: Response, next: NextFunction) {
         } else if (err) {
             return res.status(500).json({ error: 'UnknownError', message: err.message });
         }
-
         const uploadedFiles: Express.Multer.File[] = req.files as Express.Multer.File[];
-
         // Assuming you want to send the UUID of the uploaded file in the response
         if (!uploadedFiles || uploadedFiles.length === 0) {
             return res.status(400).json({ success: false, message: 'No file uploaded' });
         }
-
         return res.status(200).json({
             success: true,
             message: 'File uploaded successfully',
@@ -63,42 +60,28 @@ router.post('/', function (req: Request, res: Response, next: NextFunction) {
 });
 router.put('/:imageId', function (req: Request, res: Response, next: NextFunction) {
   upload(req, res, async function (err) {
-        if (err instanceof MulterError) {
-            return res.status(500).json({ error: 'MulterError', message: err.message });
-        } else if (err) {
-            return res.status(500).json({ error: 'UnknownError', message: err.message });
-        }
+    if (err instanceof MulterError) {
+      return res.status(500).json({ error: 'MulterError', message: err.message });
+    } else if (err) {
+      return res.status(500).json({ error: 'UnknownError', message: err.message });
+    }
 
-        const uploadedFiles: Express.Multer.File[] = req.files as Express.Multer.File[];
+    const uploadedFiles: Express.Multer.File[] = req.files as Express.Multer.File[];
 
-        if (!uploadedFiles || uploadedFiles.length === 0) {
-            return res.status(400).json({ success: false, message: 'No file uploaded' });
-        }
+    if (!uploadedFiles || uploadedFiles.length === 0) {
+      return res.status(400).json({ success: false, message: 'No file uploaded' });
+    }
 
-        const imageId = req.params.imageId;
-        const uploadedImagesPath = './uploaded_images';
-        const imagePath = `${uploadedImagesPath}/${imageId}`;
+    const imageId = req.params.imageId;
+    const uploadedImagesPath = './uploaded_images';
+    const imagePath = `${uploadedImagesPath}/${uuidv4()}`;
 
-        try {
-          // Check if the file exists
-          if (fs.existsSync(imagePath)) {
-            // Remove the existing image file
-            fs.unlinkSync(imagePath);
-          }
-
-          // Move the new file to the destination folder with the existing imageId
-          fs.renameSync(uploadedFiles[0].path, imagePath);
-
-          return res.status(200).json({
-            success: true,
-            message: 'File updated successfully',
-            filename: uploadedFiles[0].filename,
-          });
-        } catch (error) {
-          console.error('Error:', error);
-          return res.status(500).json({ success: false, message: 'Internal server error' });
-        }
-    });
+    return res.status(200).json({
+      success: true,
+      message: 'File uploaded successfully',
+      filename: uploadedFiles[0].filename,
+  });
+  });
 });
 
 
