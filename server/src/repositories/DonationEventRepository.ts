ee045ef2fae13a098e3013d61246af3404c7ce24
Xun Yi <xunyi.work@gmail.com>
2024-01-27 22:15:57 +0800

    Added get and update method in donationevent repo

--- server/src/repositories/DonationEventRepository.ts
+++ server/src/repositories/DonationEventRepository.ts
@@ -1,9 +1,29 @@
 import { DonationEvent } from "../entities/DonationEvent";
 import { AppDataSource } from "../config/data-source";
 
-// Interacts database open close
+// A repository interacts with the data base only. 
+// There should not be any business logic in this particular segment.
+// Business logic shoudl reside in the Service layer.
+
 export class DonationEventRepository {
     async createDonationEvent(donationEvent: DonationEvent) {
         return await AppDataSource.getRepository(DonationEvent).save(donationEvent)
+    }
+
+    async getAllDonationEvents(): Promise<DonationEvent[]> {
+        return await AppDataSource.getRepository(DonationEvent).find();
+    }
+
+    async getDonationEventById(id: number): Promise<DonationEvent | null> {
+        const donationEvent = await AppDataSource.getRepository(DonationEvent).findOne({
+            where: {
+                id:id
+            }
+        });
+        return donationEvent || null;
+    }
+
+    async updateDonationEvent(donationEvent: DonationEvent) {
+        return await AppDataSource.getRepository(DonationEvent).save(donationEvent);
     }
 }
