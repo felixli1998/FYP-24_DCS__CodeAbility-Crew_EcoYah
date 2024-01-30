d0d3739062426f76d06a712ea9f2ac76081a522b
Felix <felixlidaili98@gmail.com>
2024-01-30 23:38:40 +0800

    refactor: sign up

--- client/src/services/routes.ts
+++ client/src/services/routes.ts
@@ -2,7 +2,8 @@ const BASE_URL = process.env.REACT_APP_BACKEND_URL
 
 // Users Related Routes //
 export const USER_ROUTES = {
-  RETRIEVE_BY_EMAIL: `${BASE_URL}/users/:email`
+  RETRIEVE_BY_EMAIL: `${BASE_URL}/users/:email`,
+  CREATE_USER: `${BASE_URL}/users`,
 }
 
 // General Related Routes //
