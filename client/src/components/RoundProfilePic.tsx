362932c41bba6bf0d060a13a96c7fff5032b4e8b
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2024-01-31 16:52:06 +0800

    Changed: Box component in RoundProfilePic

--- client/src/components/RoundProfilePic.tsx
+++ client/src/components/RoundProfilePic.tsx
@@ -24,6 +24,6 @@ export default function RoundProfilePic(props: RoundProfilePicProps) {
             }}
             alt={props.altText}
             src={props.pictureSrc}
-        ></Box>
+        />
     )
 };
