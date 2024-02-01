9c1ae4402eadd095f720664351834697315eeedb
Felix <felixlidaili98@gmail.com>
2024-02-02 00:40:12 +0800

    feat: added AdminHome, DonorHome

---
+++ client/src/pages/Donor/DonorHome.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const DonorHome = () => {
+  return (
+    <div>
+      <h1>Welcome to the Homepage</h1>
+      <p>This is the homepage of an authenticated donor</p>
+    </div>
+  );
+};
