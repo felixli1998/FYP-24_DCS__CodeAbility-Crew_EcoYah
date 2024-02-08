c9a5903cf1e58c71d87f4dfd736636bc5a0447f1
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2024-02-08 15:14:41 +0800

    Add: DonationEventCard component

---
+++ client/src/components/DonationEvent/DonationEventCard.tsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react';
+import AccessTimeIcon from '@mui/icons-material/AccessTime';
+
+import {
+    Button,
+    Box,
+    Container,
+    Typography,
+    Avatar,
+    ListItemAvatar,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    IconButton,
+    Card,
+    CardActions,
+    CardContent,
+    CardMedia,
+    Grid
+  } from '@mui/material';
+
+type DonationEventCardProps = {
+    name: string;
+    description: string;
+    imgSrc: string;
+    numJoined: number;
+    numHoursLeft: number;
+} 
+
+export default function DonationEventCard(props: DonationEventCardProps) {
+    return (
+        <Card sx={{ maxWidth:'400px', borderRadius: '25px'}} elevation={2}>
+            <CardMedia
+                component="img" 
+                image={props.imgSrc}
+                height='200px'
+                title={props.name}
+                sx={{padding: 2, borderRadius: '25px'}}
+            />
+            <CardContent>
+                <Grid container alignItems='center'>
+                    <Grid item xs={8}>
+                        <Typography variant='h6' sx={{fontWeight: 'bold'}}>{props.name}</Typography>
+                    </Grid>
+                    <Grid item xs={4} container justifyContent='flex-end'>
+                        {/* <CardActions> */}
+                            <Button variant='contained' size='medium'>Donate</Button>
+                        {/* </CardActions> */}
+                    </Grid>
+                </Grid>
+                
+                <Typography mt={2}>{props.description}</Typography>
+
+                <Grid container alignItems='center' sx={{marginTop: 2}}>
+                    <Grid item xs={7}>
+                        <Typography color="text.secondary">
+                            {props.numJoined > 0 ? `${props.numJoined} Donors Have Joined!` : "Be the first to make a difference!"}
+                        </Typography>
+                    </Grid>
+                    <Grid item xs={5} container justifyContent='flex-end' alignItems='center'>
+                        <AccessTimeIcon sx={{ color: 'text.secondary', marginRight: 1}}/>
+                        <Typography color='text.secondary'>
+                           {props.numHoursLeft} Hours Left
+                        </Typography>
+                    </Grid>
+                </Grid>
+            </CardContent>
+        </Card>
+    );
+}
--- client/src/pages/Donor/DonationEvents.tsx
+++ client/src/pages/Donor/DonationEvents.tsx
@@ -1,44 +1,70 @@
 import {useState, useEffect } from "react";
+import DonationEventCard from "../../components/DonationEvent/DonationEventCard";
+import exampleImg from "../../assets/Reward.png";
+import {
+    Box,
+    Container,
+    Typography,
+    Avatar,
+    ListItemAvatar,
+    List,
+    ListItem,
+    ListItemButton,
+    ListItemText,
+    IconButton
+  } from '@mui/material';
 
 export default function DonationEvents() {
-    const [events, setEvents] = useState([]);
-    const [errorFetchingEvents, setErrorFetchingEvents] = useState(false);
+    // const [events, setEvents] = useState([]);
+    // const [errorFetchingEvents, setErrorFetchingEvents] = useState(false);
 
-    const getAllEvents = async () => {
-        try {
-            const response = await fetch('http://localhost:5000/api/events');
-            const data = await response.json();
-            return data;
-        } catch (error) {
-            console.error('Error:', error);
-            throw error;
-        }
-    }
+    // const getAllEvents = async () => {
+    //     try {
+    //         const response = await fetch('http://localhost:5000/api/events');
+    //         const data = await response.json();
+    //         return data;
+    //     } catch (error) {
+    //         console.error('Error:', error);
+    //         throw error;
+    //     }
+    // }
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const res = await getAllEvents();
-                setEvents(res);
-            } catch (error) {
-                console.error('Error:', error);
-                setErrorFetchingEvents(true);
-            }
-        }
-        fetchData();
-    }, []);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         try {
+    //             const res = await getAllEvents();
+    //             setEvents(res);
+    //         } catch (error) {
+    //             console.error('Error:', error);
+    //             setErrorFetchingEvents(true);
+    //         }
+    //     }
+    //     fetchData();
+    // }, []);
 
     return (
-        <div>
-            <h1>Donation Events</h1>
-            <ul>
-                {/* {events.map((event) => (
-                    <li key={event.id}>
-                        <h2>{event.name}</h2>
-                        <p>{event.description}</p>
-                    </li>
-                ))} */}
-            </ul>
-        </div>
+        <Box sx={{marginTop: 3, marginX: 3}}>
+            <Typography variant='h5' sx={{fontWeight: 'bold', marginBottom: 2}}>Donation of the Week</Typography>
+            
+            <DonationEventCard
+                name='Example'
+                description='This is an example description'
+                imgSrc="https://picsum.photos/200/300"
+                numJoined={8}
+                numHoursLeft={8}
+            />
+            <Typography sx={{fontWeight: 'bold', marginY: 2}}>Donation Categories</Typography>
+        </Box>
+        // <div>
+        //     <h1>Donation Events</h1>
+        //     <ul>
+        //         {events.map((event) => (
+        //             <li key={event.id}>
+        //                 <h2>{event.name}</h2>
+        //                 <p>{event.description}</p>
+        //             </li>
+        //         ))}
+        //     </ul>
+        // </div>
     );
 }
