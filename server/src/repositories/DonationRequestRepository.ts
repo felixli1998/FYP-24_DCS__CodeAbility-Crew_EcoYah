c075a0066c77a1169f41233898c975b4b010b06b
Felix <felixlidaili98@gmail.com>
2024-02-11 17:12:25 +0800

    feat: added code to retrieve by date

--- server/src/repositories/DonationRequestRepository.ts
+++ server/src/repositories/DonationRequestRepository.ts
@@ -1,5 +1,10 @@
+// External Imports
+import { startOfDay, endOfDay } from 'date-fns';
+
+// Internal imports
 import { DonationRequest } from '../entities/DonationRequest';
 import { AppDataSource } from '../config/data-source';
+import { Between } from 'typeorm';
 
 export class DonationRequestRepository {
   // TODO: This was created during model creation. Feel free to expand upon it as needed
@@ -9,8 +14,17 @@ export class DonationRequestRepository {
     );
   }
 
-  // TODO: This was created during model creation. Feel free to expand upon it as needed
   async cancelDonationRequest(id: number) {
     return await AppDataSource.getRepository(DonationRequest).softDelete(id);
+  }
+
+  // TODO: This was created during model creation. Feel free to expand upon it as needed
+  async retrieveDonationRequestByDate(date: Date) {
+    const start = startOfDay(date);
+    const end = endOfDay(date);
+
+    return await AppDataSource.getRepository(DonationRequest).find({
+      where: { dropOffDate: Between(start, end) },
+    });
   }
 }
