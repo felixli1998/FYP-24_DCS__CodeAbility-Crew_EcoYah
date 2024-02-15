2395900ff736ba486a0be93d27d840d4053752c7
xun <xunyi.work@gmail.com>
2024-02-15 16:49:48 +0800

    add getActiveCompleted donation request from user to service

--- server/src/services/DonationRequestService.ts
+++ server/src/services/DonationRequestService.ts
@@ -1,11 +1,24 @@
 import { DonationRequest } from '../entities/DonationRequest';
 import { DonationRequestRepository } from '../repositories/DonationRequestRepository';
-
 export class DonationRequestService {
   private donationRequestRepository: DonationRequestRepository;
 
   constructor(donationRequestRepository: DonationRequestRepository) {
     this.donationRequestRepository = donationRequestRepository;
+  }
+
+  async getActiveDonationRequestFromUser(user_id:number, page: number = 1) {
+    return await this.donationRequestRepository.getActiveDonationRequestFromUser(
+      user_id,
+      page
+    );
+  }
+
+  async getCompletedDonationRequestFromUser(user_id:number, page: number = 1) {
+    return await this.donationRequestRepository.getCompletedDonationRequestFromUser(
+      user_id,
+      page
+    );
   }
 
   async createDonationRequest(donationRequest: DonationRequest) {
