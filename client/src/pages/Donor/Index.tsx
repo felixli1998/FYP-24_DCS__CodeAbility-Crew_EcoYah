fdbd6fa906f2323565da1ccc9c6553db4778972f
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2024-02-21 16:07:31 +0800

    Removed unused codes in frontend Index.tsx and fixed bug in backend data-sourcet.ts

--- client/src/pages/Donor/Index.tsx
+++ client/src/pages/Donor/Index.tsx
@@ -3,13 +3,6 @@ import DonationEvents from "../../pages/Donor/DonationEvents";
 
 const Home: React.FC = () => {
   return (
-    // <div>
-    //   <h1>Welcome to the Homepage</h1>
-    //   <p>
-    //     This is a generic landing page that does not require donor to be
-    //     authenticated
-    //   </p>
-    // </div>
     <DonationEvents/>
   );
 };
