adc0895a58c6541e17a2898ad88aea71091ad2ec
Joey Lau <joeylau.2020@scis.smu.edu.sg>
2024-02-24 12:35:46 +0800

    chore: removed console logs

--- client/src/pages/Donor/DonationRequestForm.tsx
+++ client/src/pages/Donor/DonationRequestForm.tsx
@@ -84,7 +84,6 @@ export default function DonationRequestForm() {
   const handleCheckBoxChange = (
     updatedCheckedState: Record<string, boolean>
   ) => {
-    // console.log(updatedCheckedState);
     if ("omitPoints" in updatedCheckedState) {
       setDonationRequest((prevData) => ({
         ...prevData,
