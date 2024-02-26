821879ce018f473a88cbb83b3b00132cfc725c91
jack <jwzhijie@gmail.com>
2024-02-26 08:34:02 +0800

    test

--- server/src/subscriber/DonationRequest.ts
+++ server/src/subscriber/DonationRequest.ts
@@ -1,19 +1,21 @@
-import { EntitySubscriberInterface, EventSubscriber } from "typeorm"
-import { DonationRequest } from "../entities/DonationRequest"
+import { EntitySubscriberInterface, EventSubscriber } from "typeorm";
+import { DonationRequest } from "../entities/DonationRequest";
 
 @EventSubscriber()
-export class DonationRequestSubscriber implements EntitySubscriberInterface<DonationRequest> {
-    /**
-     * Indicates that this subscriber only listen to User events.
-     */
-    listenTo() {
-      return DonationRequest
-    }
+export class DonationRequestSubscriber
+  implements EntitySubscriberInterface<DonationRequest>
+{
+  /**
+   * Indicates that this subscriber only listen to User events.
+   */
+  listenTo() {
+    return DonationRequest;
+  }
 
-    /**
-     * Called before post insertion.
-     */
-    // afterUpdate(event: UpdateEvent<DonationRequest>): void | Promise<any> {
-      // TODO: UPDATE CURRENT QTY OF DONATION EVENT ITEM
-    // }
+  /**
+   * Called before post insertion.
+   */
+  // afterUpdate(event: UpdateEvent<DonationRequest>): void | Promise<any> {
+  // TODO: UPDATE CURRENT QTY OF DONATION EVENT ITEM
+  // }
 }
