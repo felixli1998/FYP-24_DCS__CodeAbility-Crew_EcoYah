fb667fa98f1039bfa0332504a7ecb9fc7b006138
xun <xunyi.work@gmail.com>
2024-03-09 00:50:49 +0800

    Added get user points

--- server/src/services/UserPointsService.ts
+++ server/src/services/UserPointsService.ts
@@ -8,6 +8,16 @@ export class UserPointsService {
     this.userPointsRepository = userPointsRepository;
   }
 
+  async getUserPoints(userId: number){
+    const userPoints = await this.userPointsRepository.getUserPointsByUserId(userId);
+    return userPoints;
+  }
+
+  // TODO: Can we rename this to addUserPoints instead? 
+  // The difference between Update, add and deduct should be
+  // Update : Does not care what the previous value is, just sets
+  // Add: Has to be non negative
+  // Deduct: Has to be non negative and less than the current value
   async creditUserPoints(userId: number, points: number) {
     const UserPoints =
       await this.userPointsRepository.getUserPointsByUserId(userId);
@@ -20,9 +30,24 @@ export class UserPointsService {
       // Implement this in the future OR we can use subscriber
 
       // Update User Points
-      await this.userPointsRepository.updateUserPoints(userId, {
+      return await this.userPointsRepository.updateUserPoints(userId, {
         points: newPoints,
       });
     }
+  }
+
+  async deductUserPoints(userId: number, points: number) {
+    if (points <= 0) {
+      throw new Error("Points to deduct must be a non zero positive number");
+    }
+    const UserPoints = await this.userPointsRepository.getUserPointsByUserId(userId);
+    if (!UserPoints){
+      throw new Error("User does not have a points account.");
+    }
+    const currentPoints = UserPoints.points;
+    if (currentPoints < points) {
+      throw new Error("User does not have enough points for this transaction.");
+    }
+    return await this.userPointsRepository.deductUserPoints(userId, points);
   }
 }
