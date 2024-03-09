7830d213d09a5ce043a3e3f68beba07070f2d6ec
Joey Lau <76724157+jlrqq@users.noreply.github.com>
2024-03-09 12:14:33 +0800

    [Frontend] Setup notification sound for cashback redemption request (#76)
    * Chatgpt assisted coding craze
    
    * Added react toastify
    
    * Added toast notification for cashback
    
    * Implemented simulate cashback on donor homepage
    
    * feat: added notification, success and reject sounds &  speech
    
    * chore: clean up codes
    
    * feat: changed toast to alert to persistent it
    
    ---------
    
    Co-authored-by: xun <xunyi.work@gmail.com>

--- client/src/pages/Donor/DonorHome.tsx
+++ client/src/pages/Donor/DonorHome.tsx
@@ -1,10 +1,33 @@
 import React from "react";
+import { Button } from "@mui/material";
+import axios from "axios";
 
 export const DonorHome = () => {
+  const redeemCashback = () => {
+    const body = {
+      id: "123",
+      name: "John",
+      points: 100,
+      timestamp: new Date().toISOString(),
+    };
+
+    axios
+      .post("http://localhost:8000/longpolling/notify", body)
+      .then((resp) => {
+        // console.log(resp);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
   return (
     <div>
       <h1>Welcome to the Homepage</h1>
       <p>This is the homepage of an authenticated donor</p>
+      {/* TODO: Button should be in the Donor Cashback Redemption Page */}
+      <Button variant="contained" onClick={redeemCashback}>
+        Redeem Cashback
+      </Button>
     </div>
   );
 };
