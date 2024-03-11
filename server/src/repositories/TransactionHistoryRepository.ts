2cec210c740a69711b5619811b65176100278685
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2024-03-11 22:46:51 +0800

    Add: getTransactionHistoryByAction method

--- server/src/repositories/TransactionHistoryRepository.ts
+++ server/src/repositories/TransactionHistoryRepository.ts
@@ -7,4 +7,30 @@ export class TransactionHistoryRepository {
   async createTransactionHistory(transactionHistory: TransactionHistory) {
     return await AppDataSource.getRepository(TransactionHistory).save(transactionHistory);
   }
+
+  /* Fetches transaction history by action (optional): 
+      - If "action == NULL", return all transaction history regardless of action and status
+      - If "action == 'credited'", return all transaction history with action "credited"
+      - If "action == 'redeemed'", return all transaction history with action "redeemed" and status "accepted"
+  */
+  async getTransactionHistoryByAction(userId: string, action?: TransactionHistory["action"]) {
+    // TODO: Refactor it to relate to user id in userPoints
+    let whereCondition: any = { userPoints : {id: userId }};
+
+    // TO DOUBLE CHECK AFTER "STATUS" IS ADDED TO THE TRANSACTION HISTORY TABLE
+    if (action){
+      whereCondition.action = action;
+      if(action === "redeemed"){
+        whereCondition.status = "accepted";
+      }
+    } 
+
+    // TODO: Attach donation request information for "All" and "Credited" action
+    return await AppDataSource.getRepository(TransactionHistory).find({
+        where: whereCondition,
+        order: {
+          createdAt: "DESC",
+        }
+    });
+  }
 }
