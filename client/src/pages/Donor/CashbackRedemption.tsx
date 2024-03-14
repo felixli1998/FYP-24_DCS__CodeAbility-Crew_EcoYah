b07011e4c2df855acb0f914867ed75683f4b7119
quentinquek <quentinquek@hotmail.com>
2024-03-14 22:19:31 +0800

    feat: add api call to for cashback redemption

--- client/src/pages/Donor/CashbackRedemption.tsx
+++ client/src/pages/Donor/CashbackRedemption.tsx
@@ -1,24 +1,49 @@
 // MUI Imports...
 import {
+  Alert,
   Box,
   Button,
-  Container,
   Grid,
-  ImageList,
-  ImageListItem,
+  Snackbar,
   Stack,
   Typography,
 } from "@mui/material";
-import BasicButton from "../../components/Button/BasicButton";
-import { FC, useState } from "react";
 import BackspaceOutlinedIcon from "@mui/icons-material/BackspaceOutlined";
-import Image from "../../components/Image/Image";
+import { SG } from "country-flag-icons/react/3x2";
+
+// Component Imports
+import BasicButton from "../../components/Button/BasicButton";
+
+// Utils Imports
+import { FC, useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+// API Imports
+import {
+  getUserByEmail,
+  makeCashbackRequest,
+} from "../../services/authenticationApi";
+import { useMutation } from "@tanstack/react-query";
 
 export default function CashbackRedemption() {
-  const [inputValue, setInputValue] = useState<string>("0");
+  const [user, setUser] = useState<any | null>(null);
+  const [inputCashbackAmt, setInputCashbackAmt] = useState<string>("0");
+  const [toast, setToast] = useState<boolean>(false);
+  const [toastMessage, setToastMessage] = useState<string>("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // Fetch Active/Complete request
+    const email = localStorage.getItem("ecoyah-email");
+    if (email) {
+      getUserByEmail(email).then((res) => {
+        setUser(res.data);
+      });
+    }
+  }, []);
 
   const handleKeyPress = (value: string) => {
-    setInputValue((prevValue) => {
+    setInputCashbackAmt((prevValue) => {
       if (value === "backspace") {
         const newValue = prevValue.slice(0, -1);
         return newValue || "0"; // Min is 0
@@ -42,6 +67,27 @@ export default function CashbackRedemption() {
     });
   };
 
+  const { mutateAsync } = useMutation({
+    mutationKey: ["cashback-request", user],
+    mutationFn: () => {
+      return makeCashbackRequest(user.id, inputCashbackAmt);
+    },
+    onSuccess: (response) => {
+      if (response && response.data) {
+        setToastMessage(response.data.message);
+        setToast(true);
+
+        if (response.status === 200) {
+          setInputCashbackAmt("0");
+          navigate("/transaction-history"); // TODO: Redirect to transaction history
+        }
+      }
+    },
+    onError: (error: any) => {
+      console.error("Error creating donation event: ", error);
+    },
+  });
+
   return (
     <Stack
       sx={{ height: "90vh" }}
@@ -58,15 +104,49 @@ export default function CashbackRedemption() {
           alignItems: "center",
         }}
       >
-        <Typography variant="h4" fontWeight={"bold"} textAlign={"center"}>
-          ${inputValue}
+        <Typography
+          variant="h3"
+          fontWeight={"bold"}
+          textAlign={"center"}
+          sx={{ marginBottom: 1 }}
+        >
+          ${inputCashbackAmt}
         </Typography>
-        <Typography variant="caption">Cashback Balance: $475</Typography>
+        <Box
+          sx={{
+            bgcolor: "#b0bec5",
+            paddingX: 2,
+            paddingY: 1,
+            borderRadius: 6,
+            display: "flex",
+          }}
+        >
+          <Box sx={{ width: 24, height: 20, mr: 1 }}>
+            <SG title="Singapore" />
+          </Box>
+          <Typography variant="subtitle2" sx={{ color: "white" }}>
+            Cashback: S$ {user && user.points}
+          </Typography>
+        </Box>
+        {user && inputCashbackAmt > user.points && (
+          <Alert
+            severity="error"
+            sx={{
+              bgcolor: "transparent",
+              marginTop: 0,
+              paddingTop: 0,
+              color: "#d50000",
+            }}
+          >
+            Current amount exceeds balance
+          </Alert>
+        )}
       </Box>
       <Box margin={2.5}>
         <BasicButton
           label={"Redeem"}
           variant={"contained"}
+          disabled={user && inputCashbackAmt > user.points}
           customStyles={{
             fontWeight: "bold",
             fontSize: 12,
@@ -77,10 +157,18 @@ export default function CashbackRedemption() {
             backgroundColor: "#004d40",
             marginBottom: 3,
           }}
-          onButtonChange={() => console.log("REDEEM")}
+          onButtonChange={() => mutateAsync()}
         />
         <NumberKeypad onKeyPress={handleKeyPress} />
       </Box>
+      <Snackbar
+        open={toast}
+        autoHideDuration={3000}
+        onClose={() => setToast(false)}
+        sx={{ width: "80%", mx: "auto", marginTop: 10 }}
+        message={toastMessage}
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+      />
     </Stack>
   );
 }
