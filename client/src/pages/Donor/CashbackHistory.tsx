434d64111977e43d1697c379b2b57d6a0524e2df
Zhi Ying Torri Ho <zhiyingho99@gmail.com>
2024-03-14 19:03:27 +0800

    Add: Error handling

--- client/src/pages/Donor/CashbackHistory.tsx
+++ client/src/pages/Donor/CashbackHistory.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useQuery } from "@tanstack/react-query";
 import "../../styles/App.css";
 import {
+    Alert,
     Chip,
     Divider ,
     Stack,
@@ -33,7 +34,7 @@ const tabs = [
 export default function CashbackHistory(){
   const [selectedTab, setSelectedTab] = useState<string>("all");
   const [user, setUser] = useState<any | null>(null);
-  const [transactions, setTransactions] = useState<any[]>([]);
+  const [errorFetchingData, setErrorFetchingData] = useState(false);
 
   const getTransactionHistoryData = async (userId: string, action?: string) => {
     try {
@@ -46,9 +47,9 @@ export default function CashbackHistory(){
           },
         },
       );
-      console.log(response.data.data.message);
       return response.data.data.message;
     } catch (error) {
+      setErrorFetchingData(true);
       console.error("Error fetching event types: ", error);
       throw new Error("Failed to fetch event types");
     }
@@ -58,7 +59,6 @@ export default function CashbackHistory(){
   useQuery({
     queryKey: ["/get-transaction-history", { status: selectedTab }],
     queryFn: async () => {
-      console.log("selectedTab", selectedTab);
       if (user) {
         if (selectedTab === "all") {
           return getTransactionHistoryData(user.id);
@@ -73,9 +73,14 @@ export default function CashbackHistory(){
   useEffect(() => {
     const email = localStorage.getItem("ecoyah-email");
     if (email) {
-      getUserByEmail(email).then((res) => {
-        setUser(res.data);
-      });
+        getUserByEmail(email).then((res) => {
+          setUser(res.data);
+        })
+        .catch((error) => {
+          setErrorFetchingData(true);
+          console.error("An error occurred while fetching user data:", error);
+        });
+        ;
     }
   }, [localStorage.getItem("ecoyah-email")]);
 
@@ -87,11 +92,18 @@ export default function CashbackHistory(){
 
   return (
     <>
-      <Container>
-          {/* <Typography variant="h5" sx={{ fontWeight: 'bold', marginTop: "10px" }}>Cashback History</Typography> */}
-      </Container>
+    {errorFetchingData ? 
+      
+      (<Container sx={{marginTop: 2}}>
+        <Alert severity="error">
+          <Typography variant="h6" sx={{ fontWeight: "bold" }}>
+            An error occurred while fetching your cashback history. Please
+            refresh and try again.{" "}
+          </Typography>
+        </Alert>
+      </Container>) : 
 
-      <Stack spacing={2} sx={{ margin: { xs: "2rem 2rem", md: "2rem 4rem" } }}>
+    (<Stack spacing={2} sx={{ margin: { xs: "2rem 2rem", md: "2rem 4rem" } }}>
         <Typography variant="h5" sx={{ fontWeight: "bold"}}>Cashback History</Typography>
 
         <ColorTabs
@@ -129,7 +141,7 @@ export default function CashbackHistory(){
                     </Typography>
                   </Stack>
                 </Grid>
-                <Grid item xs={3} sx={{ color: "#EE8F0F", textAlign: "end", alignSelf: "center"}}>
+                <Grid item xs={3} sx={{color: "#EE8F0F", textAlign: "end", alignSelf: "center"}}>
                   {transaction.action === "credited" || transaction.status === "rejected" ? "+" : "-" } ${transaction.points}
                 </Grid>
               </Grid>
@@ -140,7 +152,7 @@ export default function CashbackHistory(){
         )}
 
         <Typography sx={{ fontSize: { xs: 12, md: 20 }, textAlign: "center", marginTop: "1rem" }}>- You have reached the end of your cashback history -</Typography>
-      </Stack>
+      </Stack>)}
     </>
   )
 }
