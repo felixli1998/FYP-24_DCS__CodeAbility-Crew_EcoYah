c5b7752af12cd79248c8ed5b1f0dab61f6aa0be9
Joey Lau <joeylau.2020@scis.smu.edu.sg>
2024-03-18 13:00:09 +0800

    feat: share donation request form via whatsapp and telegram

--- client/src/routes/appRoutes.tsx
+++ client/src/routes/appRoutes.tsx
@@ -117,7 +117,7 @@ export const APP_ROUTES: AppRoutesT[] = [
     protected: true,
   },
   {
-    path: "donation-request-form",
+    path: "donation-request-form/:id/:name",
     description: "Submission of donation request form for the donor",
     element: <DonationRequestForm />,
     isAdmin: false,
